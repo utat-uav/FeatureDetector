12556bfd7f0e553641ed272704086bed66963c08
Chris Dryden <christopher.paul.dryden@gmail.com>
2018-03-22 18:41:53 -0400

    cleaned up the calculations for viability

--- main.py
+++ main.py
@@ -3,55 +3,65 @@ import os
 import math
 
 
+def determiningViability():
+	#Constants
+	#We can find the value of the angle of view from an online calculator
+	#this is one way
+	lengthline = 500
+	distancetoline = 100
+	angleofview = math.atan((lengthline/2)/distancetoline)
 
-#Constants
-#We can find the value of the angle of view from an online calculator
-#this is one way
-lengthline = 500
-distancetoline = 100
-angleofview = math.atan((lengthline/2)/distancetoline)
-
-#this is another
-#measured in millimeters
-sensorwidth = .033
-focallength = .050
-calculatedFocal = 2*math.atan((sensorwidth)/(2*focallength))
-print("Calculated Focal Length: " + str(calculatedFocal))
-
-
-
-pitch_angle = 10
-#negative_angle
-marginOfError = 1.5
-numberOfPixels = 5120
-smallestPixels = 6
-metersPerPixel = 0.008333
+	#this is another
+	#measured in millimeters
+	sensorwidth = .033
+	focallength = .050
+	calculatedFocal = 2*math.atan((sensorwidth)/(2*focallength))
+	print("Calculated Focal Length: " + str(calculatedFocal))
 
 
-#Areas
-print("The focal length: " + str(calculatedFocal*(180/math.pi)))
-#Altitude requirement calculation
-altitude = (metersPerPixel*numberOfPixels) / (2*math.tan(calculatedFocal/2))*2
-print(altitude)
-totalArea = 500000
 
-totalAreaCovered = 2*math.tan(calculatedFocal/2)*altitude/1.3333
-#meters per second
-speed = 14
-
-print("Total area covered: " + str(totalAreaCovered))
-areaPerSecond = speed*totalAreaCovered
-print(areaPerSecond)
-totalTime = totalArea/areaPerSecond
-print("Total time: " + str(totalTime))
-print("Time in minutes: " + str(totalTime/60))
-
-print(calculatedFocal)
+	pitch_angle = 10
+	#negative_angle
+	marginOfError = 1.5
+	numberOfPixels = 5120
+	smallestPixels = 6
+	metersPerPixel = 0.008333
 
 
+	#Areas
+	print("The focal length: " + str(calculatedFocal*(180/math.pi)))
+	#Altitude requirement calculation
+	altitude = (metersPerPixel*numberOfPixels) / (2*math.tan(calculatedFocal/2))*2
+	print(altitude)
+	totalArea = 500000
+
+	totalAreaCovered = 2*math.tan(calculatedFocal/2)*altitude/1.3333
+	#meters per second
+	speed = 14
+
+	print("Total area covered: " + str(totalAreaCovered))
+	areaPerSecond = speed*totalAreaCovered
+	print(areaPerSecond)
+	totalTime = totalArea/areaPerSecond
+	print("Total time: " + str(totalTime))
+	print("Time in minutes: " + str(totalTime/60))
+
+	print(calculatedFocal)
+
+
+
+def  calculateFocal():
+	#measured in millimeters
+	sensorwidth = .033
+	focallength = .050
+	calculatedFocal = 2*math.atan((sensorwidth)/(2*focallength))
+	return calculatedFocal
 
 
 def calculate_distance(elevation):
+
+	focal = calculateFocal()
+
 	widthpicture = 1920
 	heightpicture = 1080
 	pixelpointx = 1567
@@ -82,10 +92,6 @@ def calculate_distance_pitch(elevation):
 	xdistance = elevation*math.tan(math.atan(((xcomp*math.tan(calculatedFocal/2))/5000))+15*(math.pi/180))
 	print("Distance from x coordinates is: " + str(xdistance))
 
-
-#elevation_test = 500
-#calculate_distance(elevation_test)
-#calculate_distance_pitch(elevation_test)
 
 
 
